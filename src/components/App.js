89c2a56ec19d93ffa1ed0ee0233243708d227445
C P Singh <120619302+CPatgithub@users.noreply.github.com>
2023-02-27 19:47:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ const App = () => {
 
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
+      <h5 className="heading">{title}</h5>
 
       <div className="row">
         {colourConfig.map((config, index) => (
